41967e542eeb8ea1da09b8d361c40e51043e9ae9
Luu Hoang Nam <hoangnam0705@icloud.com>
2015-03-02 06:24:42 +0700

    Add service provider

---
+++ src/SettingsServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Namest\Settings;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class SettingsServiceProvider
+ *
+ * @author  Nam Hoang Luu <nam@mbearvn.com>
+ * @package Namest\Settings
+ *
+ */
+class SettingsServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+        // Publish a config file
+        $this->publishes([
+            __DIR__ . '/../config/settings.php' => config_path('settings.php')
+        ], 'config');
+
+        // Publish your migrations
+        $this->publishes([
+            __DIR__ . '/../database/migrations/' => base_path('/database/migrations')
+        ], 'migrations');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+}
