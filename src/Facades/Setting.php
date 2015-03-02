1c808489b341fd0ba084eccc42658cb699d39e91
Luu Hoang Nam <hoangnam0705@icloud.com>
2015-03-02 07:38:18 +0700

    Fix facade accessor

--- src/Facades/Setting.php
+++ src/Facades/Setting.php
@@ -2,7 +2,7 @@
 
 namespace Namest\Settings\Facades;
 
-use Illuminate\Contracts\Config\Repository as SettingContract;
+use Namest\Settings\Contracts\Repository as SettingContract;
 use Illuminate\Support\Facades\Facade;
 
 /**
